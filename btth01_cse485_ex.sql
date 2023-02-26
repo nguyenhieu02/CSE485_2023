3f8f212ff33583c954ce05b7193d4b638c6e74a8
Nguyễn Văn Hiếu <89926034+nguyenhieu02@users.noreply.github.com>
2023-02-26 22:07:06 +0700

    Update btth01_cse485_ex.sql

--- btth01_cse485_ex.sql
+++ btth01_cse485_ex.sql
@@ -208,3 +208,32 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+#a.Liệt kê các bài viết về các bài hát thuộc thể loại Nhạc trữ tình
+SELECT ten_bhat, ten_tloai FROM baiviet INNER JOIN theloai ON baiviet.ma_tloai = theloai.ma_tloai WHERE ten_tloai = "Nhạc trữ tình";
+
+#b.Liệt kê các bài viết của tác giả “Nhacvietplus”
+SELECT *FROM baiviet WHERE ma_tgia = (SELECT ma_tgia FROM tacgia WHERE ten_tgia = "Nhacvietplus");
+
+#c. Liệt kê các thể loại nhạc chưa có bài viết cảm nhận nào. 
+
+#d. Liệt kê các bài viết với các thông tin sau: mã bài viết, tên bài viết, tên bài hát, tên tác giả, tên thể loại, ngày viết.
+SELECT ma_bviet, tieude, ten_bhat, ten_tgia, ten_tloai, ngayviet FROM baiviet, tacgia, theloai WHERE baiviet.ma_tgia = tacgia.ma_tgia AND theloai.ma_tloai = baiviet.ma_tloai;
+
+#e. Tìm thể loại có số bài viết nhiều nhất 
+SELECT COUNT(ten_tloai), ten_tloai FROM theloai INNER JOIN baiviet ON theloai.ma_tloai = baiviet.ma_tloai WHERE ten_tloai = (SELECT MAX(ten_tloai) FROM theloai);
+
+#f. Liệt kê 2 tác giả có số bài viết nhiều nhất   
+
+#g. Liệt kê các bài viết về các bài hát có tựa bài hát chứa 1 trong các từ “yêu”, “thương”, “anh”,“em” 
+SELECT ten_bhat FROM baiviet WHERE (ten_bhat LIKE '%yêu%') OR (ten_bhat LIKE '%thương%') OR (ten_bhat LIKE '%anh%') OR (ten_bhat LIKE '%em%');
+
+#h. Liệt kê các bài viết về các bài hát có tiêu đề bài viết hoặc tựa bài hát chứa 1 trong các từ “yêu”, “thương”, “anh”, “em”
+SELECT ten_bhat, tieude FROM baiviet WHERE (tieude LIKE '%yêu%') OR (tieude LIKE '%thương%') OR (tieude LIKE '%anh%') OR (tieude LIKE '%em%') OR (ten_bhat LIKE '%yêu%') OR (ten_bhat LIKE '%thương%') OR (ten_bhat LIKE '%anh%') OR (ten_bhat LIKE '%em%');
+
+#i. Tạo 1 view có tên vw_Music để hiển thị thông tin về Danh sách các bài viết kèm theo Tên thể loại và tên tác giả
+
+#j. Tạo 1 thủ tục có tên sp_DSBaiViet với tham số truyền vào là Tên thể loại và trả về danh sách Bài viết của thể loại đó. Nếu thể loại không tồn tại thì hiển thị thông báo lỗi.
+
+#k. Thêm mới cột SLBaiViet vào trong bảng theloai. Tạo 1 trigger có tên tg_CapNhatTheLoai để khi thêm/sửa/xóa bài viết thì số lượng bài viết trong bảng theloai được cập nhật theo.
+#l. Bổ sung thêm bảng Users để lưu thông tin Tài khoản đăng nhập và sử dụng cho chức năng Đăng nhập/Quản trị trang web.
